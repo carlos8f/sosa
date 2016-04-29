f31d1e8e66020ff2e4b4c17c0bd99214d4845afb
Carlos Rodriguez <carlos@s8f.org>
2016-04-29 12:35:01 -0700

    add type checks

--- api.js
+++ api.js
@@ -6,9 +6,14 @@ module.exports = function (store, options) {
         opts = {};
       }
       opts || (opts = {});
+      if (!id) {
+        var err = new Error('must provide id to load');
+        err.id = id;
+        return cb(err);
+      }
       store.load(id, opts, function (err, obj) {
         if (err) return cb(err);
-        if (options.load && opts.hooks !== false && obj) {
+        if (options.load && opts.hooks !== false && obj !== null) {
           options.load.call(api, obj, opts, cb);
         }
         else cb(null, obj);
@@ -20,23 +25,37 @@ module.exports = function (store, options) {
         opts = {};
       }
       opts || (opts = {});
+      if (!id) {
+        var err = new Error('must provide id to save');
+        err.id = id;
+        err.obj = obj;
+        return cb(err);
+      }
+      if (typeof obj === 'undefined' || obj === null) {
+        var err = new Error('must provide obj to save');
+        err.id = id;
+        err.obj = obj;
+        return cb(err);
+      }
       if (options.save && opts.hooks !== false) {
-        options.save.call(api, obj, opts, function (err, obj) {
+        options.save.call(api, obj, opts, function (err, retObj) {
           if (err) {
             err.saved = false;
             return cb(err);
           }
-          withHooks(obj);
+          withHooks(typeof retObj === 'undefined' ? obj : retObj);
         });
       }
       else withHooks(obj);
 
       function withHooks (obj) {
-        store.save(id, obj, opts, function (err, obj) {
+        store.save(id, obj, opts, function (err, retObj) {
           if (err) {
             err.saved = false;
+            err.obj = obj;
             return cb(err);
           }
+          if (typeof retObj !== 'undefined') obj = retObj;
           if (options.afterSave && opts.hooks !== false) {
             options.afterSave.call(api, obj, opts, function (err) {
               if (err) {
@@ -57,12 +76,17 @@ module.exports = function (store, options) {
         opts = {};
       }
       opts || (opts = {});
+      if (!id) {
+        var err = new Error('must provide id to destroy');
+        err.id = id;
+        return cb(err);
+      }
       store.destroy(id, opts, function (err, obj) {
         if (err) {
           err.saved = false;
           return cb(err);
         }
-        if (options.destroy && opts.hooks !== false && obj) {
+        if (options.destroy && opts.hooks !== false && typeof obj !== 'undefined' && obj !== null) {
           options.destroy.call(api, obj, opts, function (err) {
             if (err) {
               err.saved = true;
